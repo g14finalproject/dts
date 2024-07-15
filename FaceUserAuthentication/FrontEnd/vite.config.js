dbfdbe99c482e7e9de74746656201f9861dd8fbd
Ramanaraj2000 <vijayanramanaraj@gmail.com>
2024-07-16 01:11:01 +0530

    Initial commit

---
+++ FaceUserAuthentication/FrontEnd/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+    server: {
+      proxy: {
+        "/register": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+          secure: false,
+        },
+        "/getUsers": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
+
+  return config;
+});
